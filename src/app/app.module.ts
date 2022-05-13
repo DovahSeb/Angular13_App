4064114235cd7d646162928dc269d0aa7d1e13ba
Sebastien Larue <sebastien.larue@ict.gov.sc>
2022-05-13 10:11:49 +0400

    Implemented ngxPermissions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Angular Material Imports
 import { MatSliderModule } from '@angular/material/slider'
+import { NgxPermissionsModule } from 'ngx-permissions';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MatSliderModule } from '@angular/material/slider'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    NgxPermissionsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
